26924c8249a56e65b5f0b0faef463bbd47722c15
Bilal Karakan <bilalkaraka36@gmail.com>
2022-04-03 03:02:46 +0300

    03.04.2022

---
+++ Classes/classes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Updated();
+
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Bilal";
+            customer.LastName = "Karakan";
+            customer.City = "İzmir";
+
+            Customer customer2 = new Customer
+            {
+                Id = 2,
+                FirstName = "Elif",
+                LastName = "Durur",
+                City = "İzmir"
+            };
+
+            Console.WriteLine(customer.FirstName);
+            Console.WriteLine(customer2.City);
+
+            Console.ReadKey();
+        }
+    }
+}
