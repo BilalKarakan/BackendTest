454d9871cd07026517e03be8e5e5e3b196732387
Bilal Karakan <bilalkaraka36@gmail.com>
2022-05-11 22:07:20 +0300

    Düzeltmeler Yapıldı....

---
+++ Interfaces/interfaces/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+            interface IPerson
+        {
+            int Id { get; set; }
+            string FirstName { get; set; }
+            string LastName { get; set; }
+        }
+        class Customer:IPerson
+        {
+            int Id { get; set; }
+            string FirstName { get; set; }
+            string LastName { get; set; }
+
+            string Address { get; set; }
+        }
+        class Student:IPerson
+        {
+            int Id { get; set; }
+            string FirstName { get; set; }
+            string LastName { get; set; }
+
+            string Department { get; set; }
+        }
+        
+    }
+}
